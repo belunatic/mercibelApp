05948226eb6324ce7fe41ae0c22e8786e657f658
belunatic <abelluvyou@yahoo.com>
2025-04-01 14:46:01 -0700

    add showRegister functional to work

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -5,74 +5,79 @@ import axios from "axios";
 import "../css/style.css";
 
 const Login = () => {
-	const { setLoggedInUser } = UseAuthContext();
+  const { setLoggedInUser, setShowRegister } = UseAuthContext();
 
-	const [formData, setFormData] = useState({
-		username: "",
-		password: "",
-	});
-	const [message, setMessage] = useState("");
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
+  const [message, setMessage] = useState("");
 
-	const { username, password } = formData;
+  const { username, password } = formData;
 
-	const onChange = (e) =>
-		setFormData({ ...formData, [e.target.name]: e.target.value });
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
 
-	const onSubmit = async (e) => {
-		e.preventDefault();
-		try {
-			const res = await axios.post("http://localhost:5000/user/login", {
-				username,
-				password,
-			});
-			// to make it simple i have not encrypted the items stored in local storage
-			// ensure to use encryption once you get the hang of how all of these things work
-			// as we have to protect our localStorage data in case of a breach.
-			localStorage.setItem(
-				"user",
-				JSON.stringify({
-					username: res.data.username,
-					token: res.data.token,
-					email: res.data.email,
-				})
-			);
-			console.log(res);
-			setLoggedInUser({ username: res.data.username, email: res.data.email });
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post("http://localhost:5000/user/login", {
+        username,
+        password,
+      });
+      // to make it simple i have not encrypted the items stored in local storage
+      // ensure to use encryption once you get the hang of how all of these things work
+      // as we have to protect our localStorage data in case of a breach.
+      localStorage.setItem(
+        "user",
+        JSON.stringify({
+          username: res.data.username,
+          token: res.data.token,
+          email: res.data.email,
+        }),
+      );
+      console.log(res);
+      setLoggedInUser({ username: res.data.username, email: res.data.email });
 
-			// Set success message
-			setMessage("Logged in successfully");
-		} catch (err) {
-			console.error(err.response.data);
-			// Set error message
-			setMessage("Failed to login - wrong credentials");
-		}
-	};
+      // Set success message
+      setMessage("Logged in successfully");
+    } catch (err) {
+      console.error(err.response.data);
+      // Set error message
+      setMessage("Failed to login - wrong credentials");
+    }
+  };
 
-	return (
-		<div className="auth-form">
-			<h2>Login</h2>
-			<form onSubmit={onSubmit}>
-				<input
-					type="text"
-					placeholder="Username"
-					name="username"
-					value={username}
-					onChange={onChange}
-					required
-				/>
-				<input
-					type="password"
-					placeholder="Password"
-					name="password"
-					value={password}
-					onChange={onChange}
-					required
-				/>
-				<button type="submit">Login</button>
-			</form>
-			<p className="message">{message}</p>
-		</div>
-	);
+  return (
+    <div className="auth-form">
+      <h2>Login</h2>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="Username"
+          name="username"
+          value={username}
+          onChange={onChange}
+          required
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          name="password"
+          value={password}
+          onChange={onChange}
+          required
+        />
+        <button type="submit" className="mb-2">
+          Login
+        </button>
+        <button type="button" onClick={() => setShowRegister(true)}>
+          Register
+        </button>
+      </form>
+      <p className="message">{message}</p>
+    </div>
+  );
 };
 
 export default Login;
--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -2,68 +2,76 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "../css/style.css";
+import { UseAuthContext } from "../context/AuthContext";
 
 const Register = () => {
-	const [formData, setFormData] = useState({
-		username: "",
-		email: "",
-		password: "",
-	});
-	const [message, setMessage] = useState("");
+  const { setShowRegister } = UseAuthContext();
 
-	const { username, email, password } = formData;
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+  const [message, setMessage] = useState("");
 
-	const onChange = (e) =>
-		setFormData({ ...formData, [e.target.name]: e.target.value });
+  const { username, email, password } = formData;
 
-	const onSubmit = async (e) => {
-		e.preventDefault();
-		try {
-			await axios.post("http://localhost:5000/user/register", {
-				username,
-				email,
-				password,
-			});
-			setMessage("Registered successfully"); // Set success message
-		} catch (err) {
-			console.error("This is the error: ", err.response.data.msg);
-			setMessage(`Failed to register, ${err.response.data.msg}`); // Set error message
-		}
-	};
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
 
-	return (
-		<div className="auth-form">
-			<h2>Register</h2>
-			<form onSubmit={onSubmit}>
-				<input
-					type="text"
-					placeholder="Username"
-					name="username"
-					value={username}
-					onChange={onChange}
-					required
-				/>
-				<input
-					type="email"
-					placeholder="Email"
-					name="email"
-					value={email}
-					onChange={onChange}
-					required
-				/>
-				<input
-					type="password"
-					placeholder="Password"
-					name="password"
-					value={password}
-					onChange={onChange}
-					required
-				/>
-				<button type="submit">Register</button>
-			</form>
-			<p className="message">{message}</p>
-		</div>
-	);
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post("http://localhost:5000/user/register", {
+        username,
+        email,
+        password,
+      });
+      setMessage("Registered successfully"); // Set success message
+    } catch (err) {
+      console.error("This is the error: ", err.response.data.msg);
+      setMessage(`Failed to register, ${err.response.data.msg}`); // Set error message
+    }
+  };
+
+  return (
+    <div className="auth-form">
+      <h2>Register</h2>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="Username"
+          name="username"
+          value={username}
+          onChange={onChange}
+          required
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          name="email"
+          value={email}
+          onChange={onChange}
+          required
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          name="password"
+          value={password}
+          onChange={onChange}
+          required
+        />
+        <button type="submit" className="mb-2">
+          Register
+        </button>
+        <button type="button" onClick={() => setShowRegister(false)}>
+          Cancel
+        </button>
+      </form>
+      <p className="message">{message}</p>
+    </div>
+  );
 };
 
 export default Register;
