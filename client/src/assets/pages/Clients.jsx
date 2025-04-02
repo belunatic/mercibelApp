c15c689226e05e1fdff0e060c59b0990e99133d2
belunatic <abelluvyou@yahoo.com>
2025-04-01 18:06:16 -0700

    added placeholder fo the other pages

---
+++ client/src/assets/pages/Clients.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Clients = () => {
+  return <div>Hello Client</div>;
+};
+
+export default Clients;
---
+++ client/src/assets/pages/Products.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Products = () => {
+  return <div>hello Product</div>;
+};
+
+export default Products;
