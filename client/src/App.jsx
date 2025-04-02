2f302a92dc0de02acca50528c9021c11e71c114f
belunatic <abelluvyou@yahoo.com>
2025-04-01 17:01:13 -0700

    addded a some few package to support navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,28 +2,32 @@
 import Register from "./components/Register";
 import Login from "./components/Login";
 import { UseAuthContext } from "./context/AuthContext";
+import NavBar from "./components/NavBar";
 
 const App = () => {
   const { loggedInUser, handleLogout, showRegister } = UseAuthContext();
 
   return (
-    <div className="flex min-h-screen w-full">
-      {loggedInUser?.username ? (
-        <div>
-          <p>Welcome {loggedInUser.username}</p>
-          <button onClick={handleLogout}>Logout</button>
-        </div>
-      ) : (
-        <div className="flex w-full flex-col items-center justify-center">
-          <div className="pb-4">
-            <img
-              src="/Mercibel_Production_Logo.png"
-              alt="Mercibel Production Logo"
-            />
+    <div>
+      <NavBar />
+      <div className="flex min-h-screen w-full">
+        {loggedInUser?.username ? (
+          <div>
+            <p>Welcome {loggedInUser.username}</p>
+            <button onClick={handleLogout}>Logout</button>
           </div>
-          <div>{showRegister ? <Register /> : <Login />}</div>
-        </div>
-      )}
+        ) : (
+          <div className="flex w-full flex-col items-center justify-center">
+            <div className="pb-4">
+              <img
+                src="/Mercibel_Production_Logo.png"
+                alt="Mercibel Production Logo"
+              />
+            </div>
+            <div>{showRegister ? <Register /> : <Login />}</div>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
