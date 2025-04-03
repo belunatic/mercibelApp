e8df1def267995abaef904ffa7a88463fbbec0b5
belunatic <abelluvyou@yahoo.com>
2025-04-03 14:14:48 -0700

    made the navbar w-full

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -26,7 +26,7 @@ export default function NavBar() {
   const { loggedInUser, handleLogout } = UseAuthContext();
   return (
     <Disclosure as="nav" className="bg-gray-800">
-      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+      <div className="mx-auto w-full px-2 sm:px-8 lg:px-10">
         <div className="relative flex h-16 items-center justify-between">
           <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
             {/* Mobile menu button*/}
