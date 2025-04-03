451f9c7086dfd355df286858096a1fb91d84fe21
belunatic <abelluvyou@yahoo.com>
2025-04-03 12:01:51 -0700

    added login path

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -21,6 +21,7 @@ const App = () => {
               <Route exact path="/" element={<Dashboard />} />
               <Route path="/products" element={<Products />} />
               <Route path="/clients" element={<Clients />} />
+              <Route path="/login" element={<Login />} />
             </Routes>
           </div>
         </div>
