fee0aba61eb3161c56e596274314dd1a0d0dece7
belunatic <abelluvyou@yahoo.com>
2025-04-03 12:10:48 -0700

    added comments

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -6,9 +6,9 @@ const { ensureAuth } = require("../middleware/authMiddleware");
 
 // Register Route
 router.post("/register", authController.register);
-
+//Login route
 router.post("/login", authController.login);
-
+//get  login user in
 router.get("/", ensureAuth, authController.getUser);
 
 module.exports = router;
