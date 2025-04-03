b57163a67af9ace600e874e57a6011e6ff180287
belunatic <abelluvyou@yahoo.com>
2025-04-03 14:03:26 -0700

    addd the controller file for the products

--- server/routes/products.js
+++ server/routes/products.js
@@ -1,10 +1,9 @@
 // server/routes/products.js
 const express = require("express");
 const router = express.Router();
+const productsController = require("../controllers/productsController");
 const { ensureAuth } = require("../middleware/authMiddleware");
 
-router.get("/", ensureAuth, (req, res) => {
-	res.json({ msg: "Hello World" });
-});
+router.get("/", ensureAuth, productsController.getProducts);
 
 module.exports = router;
