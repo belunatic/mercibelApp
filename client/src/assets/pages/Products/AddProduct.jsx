d397e44bf13bcc3f1f5e3d421ff610eed9ea2ee7
belunatic <abelluvyou@yahoo.com>
2025-04-07 16:25:48 -0700

    submit button was off the form, fixed it

--- client/src/assets/pages/Products/AddProduct.jsx
+++ client/src/assets/pages/Products/AddProduct.jsx
@@ -1,6 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 const AddProduct = () => {
+  const [productName, setProductName] = useState("");
+  const [productPrice, setProductPrice] = useState(1);
+  const [productDesc, setProductDesc] = useState("");
+
+  const handleSubmit = async (e) => {
+    try {
+      e.preventDefault();
+      console.log("Hello");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div className="relative m-4 sm:m-10">
       <div className="flex items-start justify-between rounded-t border-b p-5">
@@ -8,11 +21,11 @@ const AddProduct = () => {
       </div>
 
       <div className="space-y-6 p-6">
-        <form action="#">
+        <form onSubmit={handleSubmit}>
           <div className="grid grid-cols-6 gap-6">
             <div className="col-span-6 sm:col-span-3">
               <label
-                for="product-name"
+                htmlFor="product-name"
                 className="mb-2 block text-sm font-medium text-gray-900"
               >
                 Product Name
@@ -23,13 +36,14 @@ const AddProduct = () => {
                 id="product-name"
                 className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 shadow-sm focus:border-cyan-600 focus:ring-cyan-600 sm:text-sm"
                 placeholder="Cassava Flour 25 kg”"
-                required=""
+                required
+                onChange={(e) => setProductName(e.target.value)}
               />
             </div>
 
             <div className="col-span-6 sm:col-span-3">
               <label
-                for="price"
+                htmlFor="price"
                 className="mb-2 block text-sm font-medium text-gray-900"
               >
                 Price
@@ -40,12 +54,13 @@ const AddProduct = () => {
                 id="price"
                 className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 shadow-sm focus:border-cyan-600 focus:ring-cyan-600 sm:text-sm"
                 placeholder="3800"
-                required=""
+                onChange={(e) => setProductPrice(e.target.value)}
+                required
               />
             </div>
             <div className="col-span-full">
               <label
-                for="product-details"
+                htmlFor="product-details"
                 className="mb-2 block text-sm font-medium text-gray-900"
               >
                 Product Details
@@ -54,20 +69,17 @@ const AddProduct = () => {
                 id="product-details"
                 rows="6"
                 className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-4 text-gray-900 focus:border-cyan-600 focus:ring-cyan-600 sm:text-sm"
+                onChange={(e) => setProductDesc(e.target.value)}
                 placeholder="Details"
               ></textarea>
             </div>
           </div>
+          <div className="rounded-b border-t border-gray-200 py-6">
+            <button className="cursor-pointer rounded-lg bg-green-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-green-600 focus:ring-4 focus:ring-cyan-200">
+              Create
+            </button>
+          </div>
         </form>
-      </div>
-
-      <div className="rounded-b border-t border-gray-200 p-6">
-        <button
-          className="rounded-lg bg-green-700 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-green-600 focus:ring-4 focus:ring-cyan-200"
-          type="submit"
-        >
-          Create
-        </button>
       </div>
     </div>
   );
