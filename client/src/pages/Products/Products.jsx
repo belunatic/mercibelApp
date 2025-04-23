0e5c501c375ec85971d7a26385583c9f09d43eb0
belunatic <abelluvyou@yahoo.com>
2025-04-22 18:33:13 -0700

    fix: correct class attribute usage and improve table styling in Products component

--- client/src/pages/Products/Products.jsx
+++ client/src/pages/Products/Products.jsx
@@ -21,57 +21,57 @@ const Products = () => {
 
   const displayProducts = () => {
     return (
-      <div class="flex flex-col">
-        <div class="-m-1.5 overflow-x-auto">
-          <div class="inline-block min-w-full p-1.5 align-middle">
-            <div class="overflow-hidden">
-              <table class="min-w-full divide-y divide-gray-200 dark:divide-neutral-700">
+      <div className="flex flex-col">
+        <div className="-m-1.5 overflow-x-auto">
+          <div className="inline-block min-w-full p-1.5 align-middle">
+            <div className="overflow-hidden">
+              <table className="min-w-full divide-y divide-gray-200 dark:divide-neutral-700 dark:bg-gray-800">
                 <thead>
                   <tr>
                     <th
                       scope="col"
-                      class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase dark:text-neutral-500"
+                      className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase dark:text-neutral-300"
                     >
                       Name
                     </th>
                     <th
                       scope="col"
-                      class="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase dark:text-neutral-500"
+                      className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase dark:text-neutral-300"
                     >
                       Price
                     </th>
                     <th
                       scope="col"
-                      class="hidden px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase sm:hidden dark:text-neutral-500"
+                      className="hidden px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase sm:hidden dark:text-neutral-300"
                     >
                       Description
                     </th>
                     <th
                       scope="col"
-                      class="px-6 py-3 text-end text-xs font-medium text-gray-500 uppercase dark:text-neutral-500"
+                      className="px-6 py-3 text-end text-xs font-medium text-gray-500 uppercase dark:text-neutral-300"
                     >
                       Action
                     </th>
                   </tr>
                 </thead>
-                <tbody class="divide-y divide-gray-200 dark:divide-neutral-700">
+                <tbody className="divide-y divide-gray-200 dark:divide-neutral-700">
                   {data.map((item) => (
-                    <tr class="hover:bg-gray-100 dark:hover:bg-neutral-700">
-                      <td class="px-6 py-4 text-sm font-medium whitespace-nowrap text-gray-800 dark:text-neutral-200">
+                    <tr className="hover:bg-gray-100 dark:hover:bg-neutral-700">
+                      <td className="px-6 py-4 text-sm font-medium whitespace-nowrap text-gray-800 dark:text-neutral-200">
                         {item.name}
                       </td>
-                      <td class="px-6 py-4 text-sm whitespace-nowrap text-gray-800 dark:text-neutral-200">
+                      <td className="px-6 py-4 text-sm whitespace-nowrap text-gray-800 dark:text-neutral-200">
                         {item.price}
                       </td>
-                      <td class="hidden px-6 py-4 text-sm whitespace-nowrap text-gray-800 sm:hidden dark:text-neutral-200">
+                      <td className="hidden px-6 py-4 text-sm whitespace-nowrap text-gray-800 sm:hidden dark:text-neutral-200">
                         {item.description.length > 25
                           ? `${item.description.slice(0, 25)}...`
                           : item.description}
                       </td>
-                      <td class="px-6 py-4 text-end text-sm font-medium whitespace-nowrap">
+                      <td className="px-6 py-4 text-end text-sm font-medium whitespace-nowrap">
                         <button
                           type="button"
-                          class="inline-flex items-center gap-x-2 rounded-lg border border-transparent text-sm font-semibold text-blue-600 hover:text-blue-800 focus:text-blue-800 focus:outline-hidden disabled:pointer-events-none disabled:opacity-50 dark:text-blue-500 dark:hover:text-blue-400 dark:focus:text-blue-400"
+                          className="inline-flex items-center gap-x-2 rounded-lg border border-transparent text-sm font-semibold text-blue-600 hover:text-blue-800 focus:text-blue-800 focus:outline-hidden disabled:pointer-events-none disabled:opacity-50 dark:text-blue-500 dark:hover:text-blue-400 dark:focus:text-blue-400"
                         >
                           Delete
                         </button>
@@ -87,7 +87,10 @@ const Products = () => {
     );
   };
   return (
-    <div div className="mx-auto w-full px-2 sm:px-8 lg:px-10 dark:bg-gray-800">
+    <div
+      div
+      classNameName="mx-auto w-full px-2 sm:px-8 lg:px-10 dark:bg-gray-800"
+    >
       {loading ? "Loading..." : displayProducts()}
     </div>
   );
