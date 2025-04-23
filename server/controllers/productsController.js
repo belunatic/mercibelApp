6011a6952614a5dc47c1e39445e1f2bef0f36aab
belunatic <abelluvyou@yahoo.com>
2025-04-22 18:20:53 -0700

    implement edit, delete, and get product by ID functionalities in productsController

--- server/controllers/productsController.js
+++ server/controllers/productsController.js
@@ -13,4 +13,36 @@ module.exports = {
 			res.status(201).json({ msg: "Product Added" });
 		}
 	}),
+	editProduct: asyncHandler(async (req, res) => {
+		console.log("These are", req.body, req.user.id);
+		let data = await Product.findByIdAndUpdate(req.params.id, {
+			$set: {
+				...req.body,
+				lastUpdateDate: new Date(),
+				lastUpdateBy: req.user.id,
+			},
+		});
+		console.log("This is data", data);
+		if (data) {
+			res.status(201).json({ msg: "Product Updated" });
+		} else {
+			res.status(404).json({ msg: "Product not found" });
+		}
+	}),
+	deleteProduct: asyncHandler(async (req, res) => {
+		let data = await Product.findByIdAndDelete(req.params.id);
+		if (data) {
+			res.status(201).json({ msg: "Product Deleted" });
+		} else {
+			res.status(404).json({ msg: "Product not found" });
+		}
+	}),
+	getProductById: asyncHandler(async (req, res) => {
+		let data = await Product.findById(req.params.id);
+		if (data) {
+			res.status(200).json(data);
+		} else {
+			res.status(404).json({ msg: "Product not found" });
+		}
+	}),
 };
