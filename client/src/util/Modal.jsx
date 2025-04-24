013e3127c848c404da36128db7a4339a5003c5ac
belunatic <abelluvyou@yahoo.com>
2025-04-24 14:57:14 -0700

    fix: refactor Modal component structure for improved layout and styling

--- client/src/util/Modal.jsx
+++ client/src/util/Modal.jsx
@@ -15,37 +15,52 @@ const Modal = ({ openModal, closeModal, children, confirmFunction }) => {
   }, [openModal]);
 
   return (
-    <dialog
-      ref={ref}
-      onCancel={closeModal}
-      className="fixed inset-0 flex h-screen w-screen items-center justify-center bg-transparent p-4 backdrop-blur-sm"
-    >
-      <div className="w-full max-w-md rounded-lg bg-white p-6 shadow-lg">
-        <div className="text-end">
-          <button
-            onClick={closeModal}
-            className="cursor-pointer pb-2 text-red-500"
-          >
-            X
-          </button>
+    <div className="fixed inset-0 flex h-screen w-screen items-center justify-center bg-transparent p-4 backdrop-blur-[1px]">
+      <dialog
+        ref={ref}
+        onCancel={closeModal}
+        className="m-auto flex h-full w-full items-center justify-center bg-transparent"
+      >
+        <div className="w-full max-w-md rounded-lg bg-white p-6 shadow-lg dark:bg-gray-800 dark:text-white">
+          <div className="text-end">
+            <button
+              onClick={closeModal}
+              className="cursor-pointer pb-2 font-semibold text-red-500"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6 18 18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
+          </div>
+          <div className="pt-2 pb-4">{children}</div>
+          <div className="mt-4 flex justify-end space-x-4">
+            <button
+              className="cursor-pointer rounded bg-red-500 px-4 py-2 text-white"
+              onClick={confirmFunction}
+            >
+              Delete
+            </button>
+            <button
+              className="cursor-pointer rounded bg-gray-500 px-4 py-2 text-white"
+              onClick={closeModal}
+            >
+              Cancel
+            </button>
+          </div>
         </div>
-        <div className="pt-2 pb-4">{children}</div>
-        <div className="mt-4 flex justify-end space-x-4">
-          <button
-            className="cursor-pointer rounded bg-red-500 px-4 py-2 text-white"
-            onClick={confirmFunction}
-          >
-            Delete
-          </button>
-          <button
-            className="cursor-pointer rounded bg-gray-500 px-4 py-2 text-white"
-            onClick={closeModal}
-          >
-            Cancel
-          </button>
-        </div>
-      </div>
-    </dialog>
+      </dialog>
+    </div>
   );
 };
 
