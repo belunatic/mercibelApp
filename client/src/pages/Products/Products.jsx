bfffd9412b3551493182c2aac514f34ada40adc2
belunatic <abelluvyou@yahoo.com>
2025-04-24 16:24:26 -0700

    fix: add navigation button to Products page for adding new products

--- client/src/pages/Products/Products.jsx
+++ client/src/pages/Products/Products.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import React, { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import Modal from "../../util/Modal";
 import ToastUtil from "../../util/ToastUtil";
 import { UseAuthContext } from "../../context/AuthContext";
@@ -148,6 +148,27 @@ const Products = () => {
         {loading ? "Loading..." : displayProducts()}
       </div>
       <ToastUtil />
+
+      <div class="fixed right-4 bottom-20">
+        <NavLink to="/addProduct">
+          <button class="rounded-full bg-green-700 px-2 py-2 font-bold text-white shadow-lg hover:bg-green-600">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="size-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M12 4.5v15m7.5-7.5h-15"
+              />
+            </svg>
+          </button>
+        </NavLink>
+      </div>
     </>
   );
 };
