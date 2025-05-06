8c6854b27ee0eb765cd633c47bd8f1ceab7cfe83
belunatic <abelluvyou@yahoo.com>
2025-05-06 16:21:33 -0700

    feat: enhance AddCustomers component with initial form state and axios import

--- client/src/pages/Customers/AddCustomers.jsx
+++ client/src/pages/Customers/AddCustomers.jsx
@@ -1,10 +1,14 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const AddCustomers = () => {
-  const [formData, setFormData] = useState({
+  //initate and reset form data after submit
+  const initialFormState = {
     customerType: "Retailer",
     customerCountry: "Tanzania",
-  });
+  };
+
+  const [formData, setFormData] = useState(initialFormState);
 
   const handleChange = (event) => {
     const { name, value } = event.target;
@@ -12,12 +16,29 @@ const AddCustomers = () => {
     setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
   };
 
+  // const addCustomer = async () => {
+  //   try {
+  //     const res = await axios.post("http://localhost:5000/products/", {
+  //       name: productName,
+  //       price: productPrice,
+  //       description: productDesc,
+  //       lastUpdateBy: loggedInUser.id,
+  //       lastUpdateDate: new Date(),
+  //     });
+  //   } catch (err) {
+  //     console.log(err);
+  //   }
+  // };
+
   const handleSubmit = (event) => {
     event.preventDefault();
+
+    /*alternative way to get form data
     // const form = event.target;
     // const data = new FormData(form);
     // const formValues = Object.fromEntries(data.entries());
     // console.log("Form Data:", formValues);
+    */
     console.log("State Data:", formData);
   };
   return (
