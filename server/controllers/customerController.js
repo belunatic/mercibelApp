ba263f1edcbd4349b748efd1b494ad8c68d37ad9
belunatic <abelluvyou@yahoo.com>
2025-05-12 15:50:52 -0700

    refactor: restructure customerController methods for clarity and consistency

--- server/controllers/customerController.js
+++ server/controllers/customerController.js
@@ -13,4 +13,36 @@ module.exports = {
 			res.status(201).json({ msg: "Customer Added" });
 		}
 	}),
+	editCustomer: asyncHandler(async (req, res) => {
+		console.log("These are", req.body, req.user.id);
+		let data = await Customer.findByIdAndUpdate(req.params.id, {
+			$set: {
+				...req.body,
+				lastUpdateDate: new Date(),
+				lastUpdateBy: req.user.id,
+			},
+		});
+		console.log("This is data", data);
+		if (data) {
+			res.status(201).json({ msg: "Customer Updated" });
+		} else {
+			res.status(404).json({ msg: "Customer not found" });
+		}
+	}),
+	deleteCustomer: asyncHandler(async (req, res) => {
+		let data = await Customer.findByIdAndDelete(req.params.id);
+		if (data) {
+			res.status(201).json({ msg: "Customer Deleted" });
+		} else {
+			res.status(404).json({ msg: "Customer not found" });
+		}
+	}),
+	getCustomerById: asyncHandler(async (req, res) => {
+		let data = await Customer.findById(req.params.id);
+		if (data) {
+			res.status(200).json(data);
+		} else {
+			res.status(404).json({ msg: "Customer not found" });
+		}
+	}),
 };
