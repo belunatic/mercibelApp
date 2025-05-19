598aa74d1b91716d53563bf695717c4d4268a7eb
belunatic <abelluvyou@yahoo.com>
2025-05-19 15:21:52 -0700

    feat: create PlaceOrder component with initial structure

---
+++ client/src/pages/Orders/PlaceOrder.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PlaceOrder = () => {
+  return (
+    <div className="mx-auto w-full px-2 sm:px-8 lg:px-10 dark:bg-gray-800 dark:text-white">
+      <p>Hello</p>
+    </div>
+  );
+};
+
+export default PlaceOrder;
