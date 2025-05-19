9088ed7d9e1f03c143a92fe947af684ee4c797ac
belunatic <abelluvyou@yahoo.com>
2025-05-19 15:39:01 -0700

    added: retrieving the businesses names

--- client/src/pages/Orders/PlaceOrder.jsx
+++ client/src/pages/Orders/PlaceOrder.jsx
@@ -1,9 +1,36 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const PlaceOrder = () => {
+  const [loadingBusinesses, setLoadingBusinesses] = useState(true);
+  const [loadingProducts, setLoadingProducts] = useState(true);
+  const [products, setProducts] = useState([]);
+  const [businessesNames, setBusinessesNames] = useState([]);
+
+  useEffect(() => {
+    const fetchBusinessName = async () => {
+      try {
+        const res = await axios.get("http://localhost:5000/customers/");
+        setLoadingBusinesses(false);
+        setBusinessesNames([res.data]);
+        console.log(res);
+      } catch (err) {
+        console.log("Error Fetching Businesses", err);
+      }
+    };
+
+    fetchBusinessName();
+  }, []);
+
+  // // Businesses Name Select Input
+  // const displayBusinessesNameSelect = () => {
+  //     return {
+  //         setBusinessesNames.map(item)
+  //     }
+  // }
   return (
     <div className="mx-auto w-full px-2 sm:px-8 lg:px-10 dark:bg-gray-800 dark:text-white">
-      <p>Hello</p>
+      <p>Hello World</p>
     </div>
   );
 };
