cfdfb7e5789bb4ec14fa544e62661903f03805dc
belunatic <abelluvyou@yahoo.com>
2025-05-19 15:21:41 -0700

    feat: enhance Orders component with styling for better layout

--- client/src/pages/Orders/Orders.jsx
+++ client/src/pages/Orders/Orders.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Orders = () => {
   return (
-    <div>
+    <div className="mx-auto w-full px-2 sm:px-8 lg:px-10 dark:bg-gray-800 dark:text-white">
       <p>Hello</p>
     </div>
   );
