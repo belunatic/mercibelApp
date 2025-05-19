f4ba44b41acb07ab0c7147df63d3802922b3ba17
belunatic <abelluvyou@yahoo.com>
2025-05-19 16:28:15 -0700

    declare a value to hold the customer name when selected

--- client/src/pages/Orders/PlaceOrder.jsx
+++ client/src/pages/Orders/PlaceOrder.jsx
@@ -2,28 +2,37 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 const PlaceOrder = () => {
-  const [loadingBusinesses, setLoadingBusinesses] = useState(true);
+  const [loadingCustomer, setLoadingCustomer] = useState(true);
   const [loadingProducts, setLoadingProducts] = useState(true);
   const [products, setProducts] = useState([]);
-  const [businessesNames, setBusinessesNames] = useState([]);
+  const [customerNames, setCustomerNames] = useState([]);
+
+  //users input
+  const [customerSelected, setCustomerSelected] = useState({});
 
   useEffect(() => {
     const fetchBusinessName = async () => {
       try {
         const res = await axios.get("http://localhost:5000/customers/");
-        setLoadingBusinesses(false);
-        setBusinessesNames([...res.data]);
+        setLoadingCustomer(false);
+        setCustomerNames([...res.data]);
+        setCustomerSelected((prevState) => ({
+          ...prevState,
+          id: res.data[0]._id,
+          customerName: res.data[0].customerName,
+        }));
         console.log([...res.data]);
+        console.log(customerSelected);
       } catch (err) {
-        console.log("Error Fetching Businesses", err);
+        console.log("Error Fetching Customer", err);
       }
     };
 
     fetchBusinessName();
   }, []);
 
-  // Businesses Name Select Input
-  const displayBusinessesNameSelect = () => {
+  // Customer Name Select Input
+  const displayCustomerNameSelect = () => {
     return (
       <div className="col-span-6 sm:col-span-2">
         <label
@@ -37,13 +46,17 @@ const PlaceOrder = () => {
           id="customerName"
           className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 shadow-sm focus:border-cyan-600 focus:ring-cyan-600 sm:text-sm"
           // onChange={handleChange}
-          value={businessesNames[0].customerName || ""}
+          value={customerNames[0].customerName || ""}
           required
         >
           {/* set up the options */}
-          {businessesNames.map((item) => {
+          {customerNames.map((item) => {
             return (
-              <option key={item._id} value={item.customerName}>
+              <option
+                key={item._id}
+                data-id={item._id}
+                value={item.customerName}
+              >
                 {item.customerName}
               </option>
             );
@@ -55,9 +68,7 @@ const PlaceOrder = () => {
   return (
     <div className="mx-auto w-full px-2 sm:px-8 lg:px-10 dark:bg-gray-800 dark:text-white">
       <form>
-        {loadingBusinesses
-          ? "LoadingBusinesses"
-          : displayBusinessesNameSelect()}
+        {loadingCustomer ? "LoadingCustomer" : displayCustomerNameSelect()}
       </form>
     </div>
   );
