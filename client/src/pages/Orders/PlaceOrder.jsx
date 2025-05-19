59a05890a47413a5e54e9bb6e35b16c408b68b92
belunatic <abelluvyou@yahoo.com>
2025-05-19 16:18:26 -0700

    displayed the dropdown for business names

--- client/src/pages/Orders/PlaceOrder.jsx
+++ client/src/pages/Orders/PlaceOrder.jsx
@@ -12,8 +12,8 @@ const PlaceOrder = () => {
       try {
         const res = await axios.get("http://localhost:5000/customers/");
         setLoadingBusinesses(false);
-        setBusinessesNames([res.data]);
-        console.log(res);
+        setBusinessesNames([...res.data]);
+        console.log([...res.data]);
       } catch (err) {
         console.log("Error Fetching Businesses", err);
       }
@@ -22,15 +22,43 @@ const PlaceOrder = () => {
     fetchBusinessName();
   }, []);
 
-  // // Businesses Name Select Input
-  // const displayBusinessesNameSelect = () => {
-  //     return {
-  //         setBusinessesNames.map(item)
-  //     }
-  // }
+  // Businesses Name Select Input
+  const displayBusinessesNameSelect = () => {
+    return (
+      <div className="col-span-6 sm:col-span-2">
+        <label
+          htmlFor="customerName"
+          className="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
+        >
+          Type
+        </label>
+        <select
+          name="customerName"
+          id="customerName"
+          className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 shadow-sm focus:border-cyan-600 focus:ring-cyan-600 sm:text-sm"
+          // onChange={handleChange}
+          value={businessesNames[0].customerName || ""}
+          required
+        >
+          {/* set up the options */}
+          {businessesNames.map((item) => {
+            return (
+              <option key={item._id} value={item.customerName}>
+                {item.customerName}
+              </option>
+            );
+          })}
+        </select>
+      </div>
+    );
+  };
   return (
     <div className="mx-auto w-full px-2 sm:px-8 lg:px-10 dark:bg-gray-800 dark:text-white">
-      <p>Hello World</p>
+      <form>
+        {loadingBusinesses
+          ? "LoadingBusinesses"
+          : displayBusinessesNameSelect()}
+      </form>
     </div>
   );
 };
