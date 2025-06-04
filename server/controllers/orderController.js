1c521d18401ee0b7279b27d94a9688f885c6492d
belunatic <abelluvyou@yahoo.com>
2025-06-03 17:32:41 -0700

    added the conrtoller for the order modal

---
+++ server/controllers/orderController.js
@@ -0,0 +1,13 @@
+const Order = require("../models/Order");
+const asyncHandler = require("express-async-handler");
+
+module.exports = {
+	getOrder: asyncHandler(async (req, res) => {
+		let data = await Order.find();
+		res.json(data);
+	}),
+	newOrder: asyncHandler(async (req, res) => {
+		console.log("the body", req.body);
+		console.log("the user", req.user.id);
+	}),
+};
