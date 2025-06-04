b4cbadbd700cdfe54945cf809550cac294a55bdd
belunatic <abelluvyou@yahoo.com>
2025-06-03 17:40:59 -0700

    added the order route to index.js

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const logger = require("morgan");
 const authRoute = require("./routes/auth");
 const productRoute = require("./routes/products");
 const customerRoute = require("./routes/customers");
+const OrderRoute = require("./routes/orders");
 const connectDB = require("./config/database");
 const { errorHandler } = require("./middleware/errorMiddleware");
 const { ensureAuth } = require("./middleware/authMiddleware");
@@ -24,6 +25,7 @@ connectDB();
 app.use("/user", authRoute);
 app.use("/products", productRoute);
 app.use("/customers", customerRoute);
+app.use("/order", customerRoute);
 
 app.listen(process.env.PORT, () => {
 	console.log("let get this party started 🐯");
