dd8557642730cffb9deabff3522190054e4951b0
belunatic <abelluvyou@yahoo.com>
2025-06-03 18:22:27 -0700

    feat: associate new orders with the user who created them

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -9,5 +9,11 @@ module.exports = {
 	newOrder: asyncHandler(async (req, res) => {
 		console.log("the body", req.body);
 		console.log("the user", req.user.id);
+		req.body.createBy = req.user.id;
+		let data = Order.create(req.body);
+		if (data) {
+			console.log(data);
+			res.status(201).json({ msg: "Order Added" });
+		}
 	}),
 };
